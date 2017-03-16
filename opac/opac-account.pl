5d6d3f76d30958e1f8f2102037c41e7b5944f5b1
Johanna Raisa <johanna.raisa@gmail.com>
2017-03-16 13:27:19 +0200

    KD-1446 Vetuma online payment integration
    Adds Vetuma online payment functionality to Koha OPAC. Configuration example
    can be found from etc/vetuma-config.xml, this needs to be in koha configuration
    directory. Vetuma will automatically be enabled if it is configured.

--- opac/opac-account.pl
+++ opac/opac-account.pl
@@ -26,6 +26,9 @@ use C4::Auth;
 use C4::Output;
 use warnings;
 
+use Koha::Vetuma::Config;
+use Koha::Vetuma::Message;
+
 my $query = new CGI;
 my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
     {
@@ -63,6 +66,29 @@ foreach my $row (@$accts) {
     $num++;
 }
 
+# Vetuma on-line payments related stuff KD#1446 (if Vetuma is configured, use it)
+
+my $vetumaConfig = Koha::Vetuma::Config->new()->loadConfigXml();
+
+# minAmount is replaced with C4::Context->preference("OnlinePaymentsMinTotal")
+
+# my $minAmount = 0;
+# if(defined $vetumaConfig->{settings}->{min_amount} && $vetumaConfig->{settings}->{min_amount} > 0){
+#    $minAmount = $vetumaConfig->{settings}->{min_amount};
+# }
+
+if (defined $vetumaConfig->{settings}->{request_url}) {
+  my $messages = Koha::Vetuma::Message->new();
+  $messages->setSession($query->cookie("CGISESSID"));
+  my $messagesJson = $messages->getMessages();
+  $template->param (
+    messages_json => $messagesJson,
+    vetuma_enabled => 1
+  );
+}
+
+# Vetuma stuff ends. 
+
 $template->param(
     ACCOUNT_LINES => $accts,
     total         => sprintf( "%.2f", $total ),
@@ -71,6 +97,7 @@ $template->param(
     message_value => scalar $query->param('message_value') || q{},
     payment       => scalar $query->param('payment') || q{},
     payment_error => scalar $query->param('payment-error') || q{},
+    minimumSum => C4::Context->preference("OnlinePaymentMinTotal")
 );
 
 output_html_with_http_headers $query, $cookie, $template->output, undef, { force_no_caching => 1 };
